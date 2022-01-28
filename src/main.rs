babdfba5777adff44ca8ed8d54032cd20918425c
Ivan <ivan@cit.io>
2022-01-28 12:35:30 +0100

    add basic HTTP server and health_check route

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    // Bubble up the io::Error if we failed to bind the address
+    // Otherwise call .await on our Server
+    url_shortener::run_server()?.await
 }

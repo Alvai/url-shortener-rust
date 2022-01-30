2b69958663ce53815e0c56a6a21913436dcb6a66
Alvi <contact.ivanmilosevic@gmail.com>
2022-01-30 22:56:44 +0100

    add dynamic port and print

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use actix_web::{dev::Server, web, App, HttpServer};
+use std::net::TcpListener;
 
 mod routes;
 
@@ -6,13 +7,16 @@ use routes::greet::greet;
 use routes::health_check::health_check;
 
 pub fn run_server() -> std::io::Result<Server> {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+
+    println!("Server started on: {}", listener.local_addr().unwrap());
     let server = HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(greet))
             .route("/health_check", web::get().to(health_check))
             .route("/{name}", web::get().to(greet))
     })
-    .bind(("127.0.0.1", 8000))?
+    .listen(listener)?
     .run();
 
     Ok(server)
